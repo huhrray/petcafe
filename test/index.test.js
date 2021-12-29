f552414040074c4ae01ee2998418f5c30605fa4b
Seyeon <syhuh93@gmail.com>
2021-12-29 23:07:02 +0900

    Delete test build file

--- test/index.test.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const src_1 = require("../src");
-describe('addNum', () => {
-    it('returns arguments added together', () => {
-        expect((0, src_1.addNum)(1, 2)).toBe(3);
-    });
-});
